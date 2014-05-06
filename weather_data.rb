d5d8f1b42025025f806df4335dfcc8eeac22eabd
mikekauffman <mike.a.kauffman@gmail.com>
2014-05-06 10:41:46 -0600

    Fixed a bug in my conversion of time since epoch into day of the week format

--- weather_data.rb
+++ weather_data.rb
@@ -3,14 +3,28 @@ require 'json'
 require 'pp'
 require 'date'
 
-response = Faraday.get('http://api.openweathermap.org/data/2.5/weather?q=San$Francisco,CA&units=imperial')
-city_data = JSON.parse(response.body)
+class WeatherSearch
 
+  attr_reader :base_url
+
+  def initialize(base_url)
+    @base_url = base_url
+  end
+
+  def url
+    base_url
+  end
+
+end
+
+client = WeatherSearch.new('http://api.openweathermap.org/data/2.5/weather?q=San$Francisco,CA&units=imperial')
+response = Faraday.get(client.url)
+city_data = JSON.parse(response.body)
 
 forecast_response = Faraday.get('http://api.openweathermap.org/data/2.5/forecast/daily?q=San$Francisco,CA&units=imperial&cnt=7')
 forecast_data = JSON.parse(forecast_response.body)
 
-next_7_days_name = forecast_data["list"].map { |day| Date::DAYNAMES[(Date.jd(day["dt"])).wday] }
+next_7_days_name = forecast_data["list"].map { |day| Date::DAYNAMES[Time.at(day["dt"]).wday] }
 
 min_next_7 = forecast_data["list"].map { |day| day["temp"]["min"].to_i }
 max_next_7 = forecast_data["list"].map { |day| day["temp"]["max"].to_i }
@@ -25,6 +39,9 @@ end
 
 # DISPLAYED DATA BELOW
 
-puts "The current temperature in San Francisco is #{city_data["main"]["temp"].to_i} degrees, and it's #{city_data["weather"].first["main"].downcase}"
+puts "-"*75
+puts "The current temperature in San Francisco is #{city_data["main"]["temp"].to_i} degrees, and the sky is #{city_data["weather"].first["main"].downcase}"
+puts "-"*75
 puts "The 7-day forecast in San Francisco is:"
+puts ""
 forecast.each {|day| puts day}
